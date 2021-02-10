96f25fe38f01003f1d3cad900a483c4203d28aa8
Lucas Beneti <lucasbeneti@Cesars-MacBook-Pro.local>
2021-02-10 00:38:17 -0300

    mais alguns componentes e css do menu

---
+++ src/components/commons/Menu/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+import { MenuWrapper } from "../../../styles/MenuWrapper";
+import { Logo } from "../../../theme/Logo";
+
+export default function Menu() {
+  const links = [
+    {
+      texto: "Home",
+      url: "/",
+    },
+    {
+      texto: "perguntas frequentes",
+      url: "/faq",
+    },
+    {
+      texto: "Sobre",
+      url: "/sobre",
+    },
+  ];
+  return (
+    <MenuWrapper>
+      <MenuWrapper.LeftSide>
+        <Logo />
+      </MenuWrapper.LeftSide>
+      <MenuWrapper.CentralSide>
+        {links.map((link) => {
+          return (
+            <li>
+              <a href={link.url}>{link.texto}</a>
+            </li>
+          );
+        })}
+      </MenuWrapper.CentralSide>
+      <MenuWrapper.RightSide>
+        <button>Entrar</button>
+        <button>Cadastrar</button>
+      </MenuWrapper.RightSide>
+    </MenuWrapper>
+  );
+}

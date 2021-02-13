b15149c2ae26b0287005cb5dd805e55ce3108391
Lucas Beneti <lucasbeneti@Cesars-MacBook-Pro.local>
2021-02-13 12:10:25 -0300

    adicionado propToStyles

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -3,6 +3,7 @@ import get from "lodash/get";
 
 import { TextStyleVariantsMap } from "../../../foundation/Text";
 import { breakpointsMedia } from "../../../theme/utils/breakpointsMedia";
+import { propToStyle } from "../../../theme/utils/propToStyle";
 const ButtonGhost = css`
   color: ${(props) => {
     return get(props.theme, `colors.${props.variant}.color`);
@@ -44,4 +45,6 @@ export const Button = styled.button`
       ${TextStyleVariantsMap.paragraph1}
     `,
   })}
+  ${propToStyle("margin")}
+  ${propToStyle("display")}
 `;
--- src/foundation/Text/index.js
+++ src/foundation/Text/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
+import { propToStyle } from "../../theme/utils/propToStyle";
 
 export const TextStyleVariantsMap = {
   smallestException: css`
@@ -22,11 +23,12 @@ export const TextStyleVariantsMap = {
 
 const TextBase = styled.span`
   ${({ variant }) => TextStyleVariantsMap[variant]}
+  ${propToStyle("textAlign")}
 `;
 
-export default function Text({ tag, variant, children }) {
+export default function Text({ tag, variant, children, ...props }) {
   return (
-    <TextBase as={tag} variant={variant}>
+    <TextBase as={tag} variant={variant} {...props}>
       {children}
     </TextBase>
   );

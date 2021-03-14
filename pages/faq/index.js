8c492bd2f14f3830c7df1725eccb0e35999c4877
Lucas <lbeneti@gmail.com>
2021-03-14 16:48:38 -0300

    adicionando faq dinamico e usando o getStaticProps

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import FAQScreen from '../../src/components/screens/FAQScreen';
 
-export default function FAQPage() {
-  return <div>Página do Faq</div>;
+export default function FAQPage({ faqCategories }) {
+  // const [faqCategories, setFaqCategories] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('https://instalura-api.vercel.app/api/content/faq')
+  //     .then((response) => response.json())
+  //     .then((response) => response.data)
+  //     .then((data) => setFaqCategories(data));
+  // }, []);
+
+  return <FAQScreen faqCategories={faqCategories} />;
+}
+
+export async function getStaticProps() {
+  const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq')
+    .then((response) => response.json())
+    .then((response) => response.data);
+  return {
+    props: { faqCategories }, // will be passed to the page component as props
+  };
 }

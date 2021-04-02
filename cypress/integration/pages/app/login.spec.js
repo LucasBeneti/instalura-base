836520c3900a9a101a2d8ef285e56d6249dd32d7
Lucas Silva Beneti <lbeneti@gmail.com>
2021-04-02 15:26:47 -0300

    arrumando erros de lint

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -11,9 +11,8 @@ describe('/pages/app/login', () => {
 
     cy.url().should('include', '/app/profile');
     cy.wait('@userLogin').then((intercept) => {
-      const token = intercept.response.body.data.token;
+      const { token } = intercept.response.body.data;
       cy.getCookie('APP_TOKEN').should('exist').should('have.property', 'value', token);
-      console.log('token', intercept.response.body.data.token);
     });
   });
 });
